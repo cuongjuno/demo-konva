8905d1bec5f4f1c957a8f6e4de20650c1cdef607
duyfetch <duynguyen@fetch.tech>
2021-08-03 12:33:31 +0700

    add demo chair table circle

--- src/CollapseShape/TableCircle/index.tsx
+++ src/CollapseShape/TableCircle/index.tsx
@@ -1,14 +1,18 @@
+import { Spin } from 'antd';
 import React from 'react';
+import { useRef } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
-export interface IChairProps {
+export interface IChairProps extends IPositionProps {
     color: string;
     id: number;
+    width: number;
+    height: number;
 }
 
 export interface IPositionProps {
-    x: number;
-    y: number;
+    x?: number;
+    y?: number;
 }
 
 export interface ITableProps {
@@ -19,6 +23,8 @@ export interface ITableProps {
     tableName: string;
     position: IPositionProps;
     tableNameColor: string;
+    width: number;
+    height: number;
 }
 const TableCircle = (props: ITableProps) => {
     const getPercentTimeUsed = () => {
@@ -29,10 +35,10 @@ const TableCircle = (props: ITableProps) => {
     const { type, chairs, tableName, startTime, endTime, tableNameColor } = props;
     const [percentTimeUsed, setPercentTimeUsed] = useState(getPercentTimeUsed());
     let idInterval: any;
+
     useEffect(() => {
         if (percentTimeUsed < 100) {
             idInterval = setInterval(() => {
-                console.log("call")
                 const percent = getPercentTimeUsed();
                 setPercentTimeUsed(percent);
             }, 1000)
@@ -40,18 +46,40 @@ const TableCircle = (props: ITableProps) => {
         return () => clearInterval(idInterval);
     }, [percentTimeUsed])
 
+    useEffect(() => {
+        (props.type === "Circle" || props.type === "Rectangle") && setPositionChairs();
+    }, []);
+
+    const setPositionChairs = () => {
+        const { width: tableWidth, height: tableHeight } = props;
+        const radius = tableWidth / 2;
+        const width = tableWidth;
+        const height = tableHeight;
+        let angle = 0;
+        const step = (2 * Math.PI) / chairs.length;
+        chairs.forEach((item) => {
+            const x = Math.round(width / 2 + radius * Math.cos(angle) - item.width / 2);
+            const y = Math.round(height / 2 + radius * Math.sin(angle) - item.width / 2);
+            item.x = x;
+            item.y = y;
+            angle += step;
+        }
+        )
+    }
+
     return (
         <div className="table-container">
-            <div className="table-wrapper">
+            {(chairs[0].y as number)? <div className="table-wrapper" style={{ width: props.width, height: props.height }}>
                 <div className="table-name" style={{ color: tableNameColor }}>{tableName}</div>
-                {chairs.map((chair: IChairProps) => {
-                    return <div className={"chair-" + chair.id} style={{ backgroundColor: chair.color }} />
-                })}
                 <div className={"table " + (type === "Circle" ? "circle" : "")}>
                     <div className="time-remain" style={{ height: `${100 - percentTimeUsed}%` }}></div>
                     <div className="time-used" style={{ height: `${percentTimeUsed}%` }}></div>
                 </div>
-            </div>
+                {chairs.map((chair: IChairProps) => {
+                    return <div className="chair" style={{ backgroundColor: chair.color, left: chair.x + 'px', top: chair.y + 'px', width: chair.width, height: chair.height }} />
+                })}
+            </div> : <Spin />
+            }
             <div className="text"></div>
         </div>
     );
--- src/Demo.tsx
+++ src/Demo.tsx
@@ -8,123 +8,53 @@ const Demo = () => {
     const wrapperRef = useRef(null);
     const TableConfig: ITableProps[] = [
         {
-            type: "Rectangle",
-            startTime: new Date(new Date().getTime() - 1 * 60000),
-            endTime: new Date(new Date().getTime() + 1 * 60000),
-            tableName: "B1",
-            tableNameColor: "rgb(69, 77, 105)",
-            position: {
-                x: 0,
-                y: 0
-            },
-            chairs: [
-                {
-                    id: 1,
-                    color: "rgb(228 231 243)"
-                },
-                {
-                    id: 3,
-                    color: "rgb(228 231 243)"
-                },
-                {
-                    id: 2,
-                    color: "#4a5c6b"
-                },
-                {
-                    id: 4,
-                    color: "#4a5c6b"
-                }
-            ]
-        },
-        {
-            type: "Rectangle",
-            startTime: new Date(new Date().getTime() - 1 * 60000),
-            endTime: new Date(new Date().getTime() + 1 * 60000),
-            tableName: "B2",
-            tableNameColor: "rgb(69, 77, 105)",
-            position: {
-                x: 175,
-                y: 0
-            },
-            chairs: [
-                {
-                    id: 1,
-                    color: "rgb(228 231 243)"
-                },
-                {
-                    id: 3,
-                    color: "rgb(228 231 243)"
-                },
-            ]
-        },
-        {
-            type: "Rectangle",
-            startTime: new Date(new Date().getTime() - 1 * 60000),
-            endTime: new Date(new Date().getTime() + 1 * 60000),
-            tableName: "B3",
-            tableNameColor: "rgb(69, 77, 105)",
-            position: {
-                x: 350,
-                y: 0
-            },
-            chairs: [
-                {
-                    id: 1,
-                    color: "rgb(228 231 243)"
-                },
-                {
-                    id: 4,
-                    color: "#4a5c6b"
-                }
-            ]
-        },
-        {
             type: "Circle",
             startTime: new Date(new Date().getTime() - 1 * 60000),
             endTime: new Date(new Date().getTime() + 1 * 60000),
-            tableName: "A5",
+            tableName: "A6",
             tableNameColor: "rgb(69, 77, 105)",
             position: {
-                x: 870,
-                y: 0
+                x: 280,
+                y: 220
             },
+            width: 100,
+            height: 100,
             chairs: [
                 {
                     id: 1,
-                    color: "rgb(228 231 243)"
-                },
-                {
-                    id: 3,
-                    color: "rgb(228 231 243)"
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
                 },
                 {
                     id: 2,
-                    color: "#4a5c6b"
-                },
-                {
-                    id: 4,
-                    color: "#4a5c6b"
-                }
-            ]
-        },
-        {
-            type: "Circle",
-            startTime: new Date(new Date().getTime() - 1 * 60000),
-            endTime: new Date(new Date().getTime() + 1 * 60000),
-            tableName: "A7",
-            tableNameColor: "rgb(69, 77, 105)",
-            position: {
-                x: 1060,
-                y: 0
-            },
-            chairs: [
-                {
-                    id: 1,
-                    color: "rgb(228 231 243)"
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
                 },
                 {
                     id: 3,
-                    color: "rgb(228 231 243)"
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 4,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 5,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 6,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
                 },
             ]
         },
@@ -135,86 +65,75 @@ const Demo = () => {
             tableName: "A5",
             tableNameColor: "rgb(69, 77, 105)",
             position: {
-                x: 1250,
-                y: 0
+                x: 41,
+                y: 40
             },
-            chairs: [
-                {
-                    id: 2,
-                    color: "#4a5c6b"
-                },
-                {
-                    id: 4,
-                    color: "#4a5c6b"
-                }
-            ]
-        },
-        {
-            type: "Rectangle",
-            startTime: new Date(new Date().getTime() - 1 * 60000),
-            endTime: new Date(new Date().getTime() + 1 * 60000),
-            tableName: "A1",
-            tableNameColor: "rgb(69, 77, 105)",
-            position: {
-                x: 0,
-                y: 315
-            },
-            chairs: [ 
-                {
-                    id: 3,
-                    color: "rgb(228 231 243)"
-                },
-                {
-                    id: 4,
-                    color: "#4a5c6b"
-                }
-            ]
-        },
-        {
-            type: "Rectangle",
-            startTime: new Date(new Date().getTime() - 1 * 60000),
-            endTime: new Date(new Date().getTime() + 1 * 60000),
-            tableName: "A2",
-            tableNameColor: "red",
-            position: {
-                x: 175,
-                y: 315
-            },
+            width: 100,
+            height: 100,
             chairs: [
                 {
                     id: 1,
-                    color: "rgb(228 231 243)"
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
                 },
-                
+                {
+                    id: 2,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 3,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 4,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 5,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
             ]
         },
         {
-            type: "Rectangle",
+            type: "Circle",
             startTime: new Date(new Date().getTime() - 1 * 60000),
             endTime: new Date(new Date().getTime() + 1 * 60000),
             tableName: "A3",
             tableNameColor: "rgb(69, 77, 105)",
             position: {
-                x: 350,
-                y: 315
+                x: 280,
+                y: 40
             },
+            width: 100,
+            height: 100,
             chairs: [
                 {
                     id: 1,
-                    color: "rgb(228 231 243)"
-                },
-                {
-                    id: 3,
-                    color: "rgb(228 231 243)"
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
                 },
                 {
                     id: 2,
-                    color: "#4a5c6b"
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
                 },
                 {
-                    id: 4,
-                    color: "#4a5c6b"
-                }
+                    id: 3,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
             ]
         },
         {
@@ -224,46 +143,35 @@ const Demo = () => {
             tableName: "A4",
             tableNameColor: "rgb(69, 77, 105)",
             position: {
-                x: 870,
-                y: 315
+                x: 535,
+                y: 40
             },
+            width: 100,
+            height: 100,
             chairs: [
                 {
                     id: 1,
-                    color: "rgb(228 231 243)"
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 2,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
                 },
                 {
                     id: 3,
-                    color: "rgb(228 231 243)"
-                },
-                {
-                    id: 2,
-                    color: "#4a5c6b"
-                },
-                {
-                    id: 4,
-                    color: "#4a5c6b"
-                }
-            ]
-        },
-        {
-            type: "Circle",
-            startTime: new Date(new Date().getTime() - 1 * 60000),
-            endTime: new Date(new Date().getTime() + 1 * 60000),
-            tableName: "A5",
-            tableNameColor: "rgb(69, 77, 105)",
-            position: {
-                x: 1060,
-                y: 315
-            },
-            chairs: [
-                {
-                    id: 2,
-                    color: "rgb(228 231 243)"
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
                 },
                 {
                     id: 4,
-                    color: "rgb(228 231 243)"
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
                 },
             ]
         },
@@ -271,35 +179,183 @@ const Demo = () => {
             type: "Circle",
             startTime: new Date(new Date().getTime() - 1 * 60000),
             endTime: new Date(new Date().getTime() + 1 * 60000),
-            tableName: "A6",
+            tableName: "A1",
             tableNameColor: "rgb(69, 77, 105)",
             position: {
-                x: 1250,
-                y: 315
+                x: 41,
+                y: 220
             },
+            width: 100,
+            height: 100,
             chairs: [
                 {
+                    id: 1,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+            ]
+        },
+        {
+            type: "Circle",
+            startTime: new Date(new Date().getTime() - 1 * 60000),
+            endTime: new Date(new Date().getTime() + 1 * 60000),
+            tableName: "A2",
+            tableNameColor: "rgb(69, 77, 105)",
+            position: {
+                x: 535,
+                y: 220
+            },
+            width: 100,
+            height: 100,
+            chairs: [
+                {
+                    id: 1,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
                     id: 2,
-                    color: "#4a5c6b"
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+            ]
+        },
+        {
+            type: "Circle",
+            startTime: new Date(new Date().getTime() - 1 * 60000),
+            endTime: new Date(new Date().getTime() + 1 * 60000),
+            tableName: "A7",
+            tableNameColor: "rgb(69, 77, 105)",
+            position: {
+                x: 749,
+                y: 40
+            },
+            width: 200,
+            height: 200,
+            chairs: [
+                {
+                    id: 1,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 2,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 3,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
                 },
                 {
                     id: 4,
-                    color: "#4a5c6b"
-                }
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 5,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 6,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 7,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+
             ]
         },
-        
+        {
+            type: "Rectangle",
+            startTime: new Date(new Date().getTime() - 1 * 60000),
+            endTime: new Date(new Date().getTime() + 1 * 60000),
+            tableName: "A8",
+            tableNameColor: "rgb(69, 77, 105)",
+            position: {
+                x: 749,
+                y: 220
+            },
+            width: 200,
+            height: 200,
+            chairs: [
+                {
+                    id: 1,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 2,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 3,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 4,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 5,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 6,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 7,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+                {
+                    id: 8,
+                    color: "#4a5c6b",
+                    width: 30,
+                    height: 30
+                },
+            ]
+        },
+
     ]
 
     return (
         <div className="draggable-container">
             <Draggable bounds="parent">
-                <div className="draggable-area"  ref={wrapperRef}>
+                <div className="draggable-area" ref={wrapperRef}>
                     {
                         TableConfig.map((tableProps: ITableProps) => {
                             console.log(tableProps.position.x)
                             return (
-                                <Draggable onStart={(e) => e.stopPropagation()} bounds="parent" defaultPosition={{x: tableProps.position.x, y: tableProps.position.y}}>
+                                <Draggable onStart={(e) => e.stopPropagation()} bounds="parent" defaultPosition={{ x: tableProps.position.x as number, y: tableProps.position.y as number }}>
                                     <div className="draggable-wrap"><TableCircle {...tableProps} /></div>
                                 </Draggable>
                             )
